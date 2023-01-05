90a7a35ca7c7a8c38e4407235a0d3ca167e5d339
Ranto Shaw <rangershaw96@outlook.com>
2023-01-05 09:43:14 +0800

    digital

--- digital.py
+++ digital.py
@@ -4,9 +4,9 @@ import pickle as pk
 import scipy.optimize as opt
 
 
-class SmallSnowBall:
+class Digital:
 
-    def __init__(self, s0, r, sigma, t, s_kout, premium):
+    def __init__(self, s0, r, sigma, t, K, premium):
         self.n_days_per_mth = 21
         self.n_days_per_yr = 252
         self.kout_obs_days = np.arange(self.n_days_per_mth, self.n_days_per_yr + 1, self.n_days_per_mth, dtype=int)
@@ -16,15 +16,15 @@ class SmallSnowBall:
         self.v = sigma  # 波动率
         self.t = t  # 存续期，单位为年，默认为1年
         self.t_days = t * self.n_days_per_yr  # period in day
-        self.s_kout = s_kout  # 敲出价
+        self.K = K  # 敲出价
         self.premium = premium  # 期权费，0.01表示名义本金的1%
 
-        self.q = 0.05227  # dividend
+        self.q = 0.03644  # dividend
 
     def gen_trends_mc(self, rand_fp):
         """从指定的文件中读入并生成MC路径"""
-        rand = np.array(pk.load(open(rand_fp, 'rb')))
-        # rand = np.random.randn(100000, 252)
+        # rand = np.array(pk.load(open(rand_fp, 'rb')))
+        rand = np.random.randn(100000, 252)
         mu = self.r - self.q
         d_t = 1.0 / self.n_days_per_yr
         d_ln_S = (mu - 0.5 * self.v ** 2) * d_t + self.v * np.sqrt(d_t) * rand
@@ -36,16 +36,11 @@ class SmallSnowBall:
 
     def classify_trends(self, trends):
         # knock out
-        kout_bool_matrix = trends[:, self.kout_obs_days] >= self.s_kout
-        kout_flags = np.any(kout_bool_matrix, axis=1)
-        kout_days_index = kout_bool_matrix[kout_flags, :].argmax(axis=1) if kout_flags.any() else []
-        kout_days = self.kout_obs_days[kout_days_index]
-        kout_period_in_yr = kout_days / self.n_days_per_yr
+        kout_bool_matrix = trends[:, -1] >= self.K
+        n_kout = np.sum(kout_bool_matrix)
+        n_nkout = np.sum(trends[:, -1] < self.K)
 
-        # not knock out
-        n_nkout = len(trends) - len(kout_period_in_yr)
-
-        return kout_period_in_yr, n_nkout
+        return n_kout, n_nkout
 
     def valuate(self, coupon_rate, kout_period_in_yr, n_paths):
         principal = self.s0
@@ -56,20 +51,20 @@ class SmallSnowBall:
         total_premium = n_paths * principal * self.premium
         return pv_kout - total_premium
 
-    def valuate_due(self, coupon_rate, kout_period_in_yr, n_paths):
+    def valuate_due(self, coupon_rate, n_kout, n_nkout):
         principal = self.s0
 
         # knock out
-        coupons = principal * coupon_rate * kout_period_in_yr * np.exp(-self.r * kout_period_in_yr)
-        kout_premium = principal * self.premium * kout_period_in_yr * np.exp(-self.r * kout_period_in_yr)
-        pv_kout = np.sum(coupons - kout_premium)
-
-        nkout_premium = (n_paths - len(kout_period_in_yr)) * principal * self.premium * np.exp(-self.r * self.t)
-        return pv_kout - nkout_premium
+        coupons = n_kout * principal * coupon_rate * self.t * np.exp(-self.r * self.t)
+        premium = (n_kout + n_nkout) * principal * self.premium * self.t * np.exp(-self.r * self.t)
+        return coupons - premium
 
     def find_coupon_rate(self, rand_fp):
         trends = self.gen_trends_mc(rand_fp)  # 读入并生成MC路径
-        kout_period_in_yr, n_nkout = self.classify_trends(trends)  # 路径分类
-        coupon = opt.newton(self.valuate_due, 0.05, args=(kout_period_in_yr, len(trends)))
-        print(f"敲出票息率：{coupon:.6%}")
+        n_kout, n_nkout = self.classify_trends(trends)  # 路径分类
+        print(f"总路径数：{len(trends)}")
+        print(f"获得收益路径数：{n_kout}，占比{n_kout / len(trends):.2}")
+        # print(f"未获得收益路径数：{n_nkout}")
+        coupon = opt.newton(self.valuate_due, 0.05, args=(n_kout, n_nkout))
+        # print(f"敲出票息率：{coupon:.6%}")
         return coupon
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # from snowball import SnowBall
 from small_snowball import SmallSnowBall
-
+from digital import Digital
 
 # def get_sb_coupon_rate():
 #     fp = 'data/QuasiRand.pickle'
@@ -11,10 +11,16 @@ from small_snowball import SmallSnowBall
 
 def get_smallsb_coupon_rate():
     fp = 'data/QuasiRand.pickle'
-    sb = SmallSnowBall(100.0, 0.036, 0.17, 1, 100, 0.005)
+    sb = SmallSnowBall(5952.45, 0.0285, 0.17, 1, 5952.45, 0.025)
+    return sb.find_coupon_rate(fp)
+
+def get_digital_coupon_rate():
+    fp = 'data/QuasiRand.pickle'
+    sb = Digital(5952.45, 0.0235, 0.17, 1, 5952.45, 0.01)
     return sb.find_coupon_rate(fp)
 
 
 if __name__ == '__main__':
     # coupon_rate = get_sb_coupon_rate()
     smallsnowball_coupon_rate = get_smallsb_coupon_rate()
+    # digital_coupon_rate = get_digital_coupon_rate()
