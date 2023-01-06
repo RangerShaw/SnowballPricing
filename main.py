318cb4e46c5b4af26e9c52ec58aabdd93c7976c9
Ranger Shaw <rangershaw96@outlook.com>
2023-01-06 18:28:00 +0800

    xlwings

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 # from snowball import SnowBall
-from small_snowball import SmallSnowBall
+from small_snowball import *
 from digital import Digital
+import xlwings as xw
+
+sheet = xw.Book("myproject.xlsm").sheets[0]
+date_paras = sheet.range('B12').options(transpose=True, expand='table').value
+[obs_dates, _, obs_intervals] = date_paras
+print(obs_dates)
+print(obs_intervals)
+
+market_paras = sheet['C3:C7'].value
+product_paras = sheet['F3:F8'].value
+[today, S0, v, r, q] = market_paras
+[s_date, e_date, funding, coupon_rate, s_kout, principle] = product_paras
+print(market_paras)
+print(product_paras)
+sb = SmallSnowBall(5952.45, 0.0285, 0.17, 1, 5952.45, 0.025)
+pv = sb.mark_to_market(S0, s_kout, funding, coupon_rate, r, q, v, 20)
+sheet['G2'].value = pv
 
 fp = 'data/QuasiRand.pickle'
 
@@ -24,7 +41,7 @@ def get_digital_coupon_rate():
 
 def get_smallsb_M2M():
     sb = SmallSnowBall(5952.45, 0.0285, 0.17, 1, 5952.45, 0.025)
-    return sb.mark_to_market(6000, 5952.45, 0.025, 0.057, 0.0235, 0.05227, 0.17, 125)
+    return sb.mark_to_market(10000, 5952.45, 0.025, 0.057, 0.0235, 0.05227, 0.17, 251)
 
 
 if __name__ == '__main__':
--- small_snowball.py
+++ small_snowball.py
@@ -34,20 +34,17 @@ class SmallSnowBall:
 
         return S_all
 
-    def classify_trends(self, trends, kout_obs_days, s_kout, total_days):
+    def classify_trends(self, trends, kout_obs_days, s_kout, total_days, day_offset):
         # knock out
         kout_bool_matrix = trends[:, kout_obs_days] >= s_kout
         kout_flags = np.any(kout_bool_matrix, axis=1)
         kout_days_index = kout_bool_matrix[kout_flags, :].argmax(axis=1) if kout_flags.any() else []
         kout_days = kout_obs_days[kout_days_index]
-        kout_period_in_yr = kout_days / total_days
-
-        # not knock out
-        n_nkout = len(trends) - len(kout_period_in_yr)
+        kout_period_in_yr = (kout_days + day_offset) / total_days
 
         return kout_period_in_yr
 
-    def valuate(self, coupon_rate, kout_period_in_yr, n_paths):
+    def valuate_old(self, coupon_rate, kout_period_in_yr, n_paths):
         principal = self.s0
 
         coupons = principal * coupon_rate * kout_period_in_yr * np.exp(-self.r * kout_period_in_yr)
@@ -56,35 +53,68 @@ class SmallSnowBall:
         total_premium = n_paths * principal * self.premium
         return pv_kout - total_premium
 
-    def valuate_due(self, coupon_rate, r, funding, kout_period_in_yr, years, n_paths):
+    def valuate(self, coupon_rate, r, funding, kout_period_in_yr, kout_discount_yr, discount_year, n_paths):
         principal = 100
 
         # knock out
-        coupons = principal * coupon_rate * kout_period_in_yr * np.exp(-r * kout_period_in_yr)
-        kout_premium = principal * funding * kout_period_in_yr * np.exp(-r * kout_period_in_yr)
+        coupons = principal * coupon_rate * kout_period_in_yr * np.exp(-r * kout_discount_yr)
+        kout_premium = principal * funding * kout_period_in_yr * np.exp(-r * kout_discount_yr)
         pv_kout = np.sum(coupons - kout_premium)
 
         # not knock out
-        nkout_premium = (n_paths - len(kout_period_in_yr)) * principal * funding * np.exp(-r * years)
+        nkout_premium = (n_paths - len(kout_period_in_yr)) * principal * funding * np.exp(-r * discount_year)
         return pv_kout - nkout_premium
 
     def find_coupon_rate(self, rand_fp):
         trends = self.gen_trends_mc(rand_fp, self.t * self.n_days_per_yr, self.s0, self.r, self.q, self.v)
-        kout_period_in_yr = self.classify_trends(trends, self.kout_obs_days, self.s_kout, self.t_days)
-        coupon = opt.newton(self.valuate_due, 0.05, args=(self.r, self.premium, kout_period_in_yr, self.t, len(trends)))
+        kout_period_yr = self.classify_trends(trends, self.kout_obs_days, self.s_kout, self.t_days, 0)
+        coupon = opt.newton(self.valuate, 0.05,
+                            args=(self.r, self.premium, kout_period_yr, kout_period_yr, self.t, len(trends)))
         print(f"敲出票息率：{coupon:.6%}")
         return coupon
 
     def mark_to_market(self, S1, s_kout_, funding_, coupon_rate_, r_, q_, v_, days):
         if days in self.kout_obs_days and S1 >= s_kout_:
-            print(f'M2M为名义本金的：{coupon_rate_ - funding_:.6%}')
-            return
+            pv = (coupon_rate_ - funding_) * days / self.t_days
+            print(f'M2M为名义本金的：{pv:.6%}')
+            return pv
 
         kout_obs_days_ = self.kout_obs_days - days
         kout_obs_days_ = kout_obs_days_[kout_obs_days_ > 0]
         left_days = self.t * self.n_days_per_yr - days
 
         paths = self.gen_trends_mc('', left_days, S1, r_, q_, v_)
-        kout_period_in_yr = self.classify_trends(paths, kout_obs_days_, s_kout_, left_days)
-        pv = self.valuate_due(coupon_rate_, r_, funding_, kout_period_in_yr, left_days / self.n_days_per_yr, len(paths))
-        print(f'M2M为名义本金的：{pv / 100 / len(paths):.6%}')
+        kout_period_yr = self.classify_trends(paths, kout_obs_days_, s_kout_, self.t_days, days)
+        kout_discount_yr = kout_period_yr - (days / self.n_days_per_yr)
+
+        pv = self.valuate(coupon_rate_, r_, funding_, kout_period_yr, kout_discount_yr, left_days / self.n_days_per_yr,
+                          len(paths))
+        pv_ratio = pv / 100 / len(paths)
+        print(f'M2M为名义本金的：{pv_ratio:.6%}')
+        return pv_ratio
+
+
+class SmallsbM2M:
+
+    def __init__(self):
+        pass
+
+    def gen_trends_mc(self, days, S0, r, q, v):
+        rand = np.random.randn(100000, days)
+        mu = r - q
+        d_t = 1.0 / 252
+        d_ln_S = (mu - 0.5 * v ** 2) * d_t + v * np.sqrt(d_t) * rand
+        d_ln_S = np.insert(d_ln_S, 0, values=np.zeros(rand.shape[0]), axis=1)  # S0
+        ln_S = np.cumsum(d_ln_S, axis=1)
+        S_all = S0 * np.exp(ln_S)
+
+        return S_all
+
+    def m2m_365(self, principle, s0, s_kout, funding, coupon_rate, r, q, v, today, obs_days, ops_day):
+        if 0 in obs_days and s0 >= s_kout:
+            pv = (coupon_rate - funding) * principle * ops_day / 365
+            print(f'M2M价值：{pv:.6%}')
+            return pv
+
+        obs_days = obs_days[obs_days > 0]
+        paths = self.gen_trends_mc(obs_days[-1], s0, r, q, v)
