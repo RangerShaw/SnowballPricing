8e888ccf025e9ab5950df17f42342c00ffcdd480
Ranger Shaw <rangershaw96@outlook.com>
2023-02-24 16:42:47 +0800

    bt_plain

--- bt_lower_bound.py
+++ bt_lower_bound.py
@@ -8,10 +8,10 @@ underlyings = []
 
 
 def gen_intervals(closes_df: pd.DataFrame):
-    period_months = [0, 1, 2, 3, 6, 9, 12]  # 0 is 14d
-    back_years = [0.5, 1, 2, 3, 5, 9, 10]
-    date_df = closes_df['日期']
-    today = date_df.iloc[-1]
+    period_months = [0, 1, 2, 3, 6, 9, 12, 36]  # 0 is 14d
+    back_years = [0.5, 1, 2, 3, 5, 9, 10, 13]
+    date_sr = closes_df['日期']
+    today = date_sr.iloc[-1]
     print(f'today: {today}')
 
     for i, month in enumerate(period_months):
@@ -20,13 +20,13 @@ def gen_intervals(closes_df: pd.DataFrame):
 
         fst_sdate = today - (pd.DateOffset(years=back_years[i]) if i != 0 else pd.DateOffset(months=6))
         lst_sdate = today - period
-        i_fst_sdate = (closes_df['日期'] >= fst_sdate).argmax()
-        i_lst_sdate = (closes_df['日期'] <= lst_sdate).argmin()
+        i_fst_sdate = (date_sr >= fst_sdate).argmax()
+        i_lst_sdate = (date_sr <= lst_sdate).argmin()
         intervals[0] = np.arange(i_fst_sdate, i_lst_sdate)
 
-        edates = date_df.iloc[intervals[0]] + period
+        edates = date_sr.iloc[intervals[0]] + period
         edates = edates.values.reshape(len(edates), 1)
-        bools = date_df.values >= edates
+        bools = date_sr.values >= edates
         intervals[1] = np.argmax(bools, axis=1)
 
         print(len(intervals[0]))
@@ -47,31 +47,39 @@ def auto_call(closes_df: pd.DataFrame, product, call: bool):
     return n_nkout / len(intervals[0])
 
 
+def bt_plain(closes_df, product):
+    period, low_bound = np.floor(product['期限(月)']), product['下端收益触达线']
+    intervals = interval_map[period]
+    prices = closes_df[product['标的']].values
+    changes = prices[intervals[1]] / prices[intervals[0]]
+    bools = changes < low_bound if product['方向'] == '看涨' else changes > low_bound
+    n_lower = np.sum(changes < low_bound if product['方向'] == '看涨' else changes > low_bound)
+    nnull = closes_df[product['标的']].isnull().sum()
+    return n_lower / (len(changes) - closes_df[product['标的']].isnull().sum())
+
+
 def backtest(closes_df, product):
     period = np.floor(product['期限(月)'])
     intervals = interval_map[period]
     prices0 = closes_df[product['标的']].values[intervals[0]]
     prices1 = closes_df[product['标的']].values[intervals[1]]
     changes = prices1 / prices0
-    struct, low_bound = product['结构'], product['下端收益触达线']
+    struct, low_bound, direction = product['结构'], product['下端收益触达线'], product['方向']
 
     if struct == '两区间' or struct == '三区间' or struct == '价差' or struct == '触碰式':
-        n_lower = np.sum(changes < low_bound if product['方向'] == '看涨' else changes > low_bound)
-        return n_lower / len(changes)
+        return bt_plain(closes_df, product)
     elif struct == '鲨鱼鳍':
-        if product['方向'] == '两边':
+        if direction == '两边':
             return None
-            # lows = str(low_bound).split('；')
-            # n_lower = np.sum((float(lows[0]) < changes) & (changes < float(lows[1])))
         else:
-            n_lower = np.sum(changes < low_bound if product['方向'] == '看涨' else changes > low_bound)
+            n_lower = np.sum(changes < low_bound if direction == '看涨' else changes > low_bound)
         return n_lower / len(changes)
     elif struct == '区间累计':
         lows = [float(x.replace('%', 'e-2')) for x in str(low_bound).split('；')]
         n_lower = np.sum((changes < lows[0]) | (changes > lows[1]))
         return n_lower / len(changes)
     elif struct == '自动敲出':
-        return auto_call(closes_df, product, product['方向'] == '看涨')
+        return auto_call(closes_df, product, direction == '看涨')
     return None
 
 
