43b50c1c80d660a51696a78143095cb124fa8f28
Ranger Shaw <rangershaw96@outlook.com>
2023-02-24 18:03:21 +0800

    class

--- bt_lower_bound.py
+++ bt_lower_bound.py
@@ -2,104 +2,114 @@ import pandas as pd
 import numpy as np
 import openpyxl as op
 
-interval_map = {}
-structs = {'两区间', '三区间', '鲨鱼鳍', '价差', '触碰式', '区间累计', '自动敲出'}
-underlyings = []
 
+class LowerBoundTester:
 
-def gen_intervals(closes_df: pd.DataFrame):
-    period_months = [0, 1, 2, 3, 6, 9, 12, 36]  # 0 is 14d
-    back_years = [0.5, 1, 2, 3, 5, 9, 10, 13]
-    date_sr = closes_df['日期']
-    today = date_sr.iloc[-1]
-    print(f'today: {today}')
+    def __init__(self, fp):
+        self.structs = {'两区间', '三区间', '鲨鱼鳍', '价差', '触碰式', '区间累计', '自动敲出'}
+        self.bt_map = {
+            '两区间': self.bt_plain,
+            '三区间': self.bt_plain,
+            '价差': self.bt_plain,
+            '触碰式': self.bt_plain,
+            '鲨鱼鳍': self.bt_shark_fin,
+            '区间累计': self.bt_range_accrual,
+            '自动敲出': self.bt_auto_call,
+        }
+        self.products = pd.read_excel(fp, sheet_name='历史发行结构汇总', usecols='H:M', skiprows=2)
+        self.closes_df = pd.read_excel(fp, sheet_name='历史走势')
+        self.products['期限(月)'] = np.floor(self.products['期限(月)'])
+        self.underlyings = set(self.closes_df.columns)
+        self.interval_map = self.gen_intervals()
 
-    for i, month in enumerate(period_months):
-        intervals = [None, None]
-        period = pd.DateOffset(months=month) if i != 0 else pd.DateOffset(days=14)
+    def gen_intervals(self):
+        interval_map = {}
+        period_months = [0, 1, 2, 3, 6, 9, 12, 36]  # 0 is 14d
+        back_years = [0.5, 1, 2, 3, 5, 9, 10, 13]
+        date_sr = self.closes_df['日期']
+        today = date_sr.iloc[-1]
+        print(f'today: {today}')
 
-        fst_sdate = today - (pd.DateOffset(years=back_years[i]) if i != 0 else pd.DateOffset(months=6))
-        lst_sdate = today - period
-        i_fst_sdate = (date_sr >= fst_sdate).argmax()
-        i_lst_sdate = (date_sr <= lst_sdate).argmin()
-        intervals[0] = np.arange(i_fst_sdate, i_lst_sdate)
+        for i, month in enumerate(period_months):
+            intervals = [None, None]
+            period = pd.DateOffset(months=month) if i != 0 else pd.DateOffset(days=14)
 
-        edates = date_sr.iloc[intervals[0]] + period
-        edates = edates.values.reshape(len(edates), 1)
-        bools = date_sr.values >= edates
-        intervals[1] = np.argmax(bools, axis=1)
+            fst_sdate = today - (pd.DateOffset(years=back_years[i]) if i != 0 else pd.DateOffset(months=6))
+            lst_sdate = today - period
+            i_fst_sdate = (date_sr >= fst_sdate).argmax()
+            i_lst_sdate = (date_sr <= lst_sdate).argmin()
+            intervals[0] = np.arange(i_fst_sdate, i_lst_sdate)
 
-        print(len(intervals[0]))
-        interval_map[period_months[i]] = intervals
+            edates = date_sr.iloc[intervals[0]] + period
+            edates = edates.values.reshape(len(edates), 1)
+            bools = date_sr.values >= edates
+            intervals[1] = np.argmax(bools, axis=1)
 
+            print(len(intervals[0]))
+            interval_map[period_months[i]] = intervals
+        return interval_map
 
-def auto_call(closes_df: pd.DataFrame, product, call: bool):
-    period = np.floor(product['期限(月)'])
-    intervals = interval_map[period]
-    prices = closes_df[product['标的']].values
+    def bt_auto_call(self, product):
+        period = np.floor(product['期限(月)'])
+        intervals = self.interval_map[period]
+        prices = self.closes_df[product['标的']].values
 
-    i_odates = np.linspace(intervals[0], intervals[1], 13, axis=1).round().astype(int)
-    paths = prices[i_odates[:, 1:]]
-    kout_prices = (prices[i_odates[:, 0]] * product['下端收益触达线']).reshape(paths.shape[0], 1)
+        i_odates = np.linspace(intervals[0], intervals[1], 13, axis=1).round().astype(int)
+        paths = prices[i_odates[:, 1:]]
+        kout_prices = (prices[i_odates[:, 0]] * product['下端收益触达线']).reshape(paths.shape[0], 1)
 
-    nkout_bools = paths < kout_prices if call else paths > kout_prices
-    n_nkout = np.all(nkout_bools, axis=1).sum()
-    return n_nkout / len(intervals[0])
+        nkout_bools = paths < kout_prices if product['方向'] == '看涨' else paths > kout_prices
+        n_nkout = np.all(nkout_bools, axis=1).sum()
+        return n_nkout / len(intervals[0])
 
+    def bt_plain(self, product):
+        low_bound = product['下端收益触达线']
+        intervals = self.interval_map[np.floor(product['期限(月)'])]
+        prices = self.closes_df[product['标的']].values
+        prices0 = prices[intervals[0]]
+        ratios = prices[intervals[1]] / prices0
+        n_lower = np.sum(ratios < low_bound if product['方向'] == '看涨' else ratios > low_bound)
+        return n_lower / (len(ratios) - np.isnan(prices0).sum())  # 民生全球
 
-def bt_plain(closes_df, product):
-    period, low_bound = np.floor(product['期限(月)']), product['下端收益触达线']
-    intervals = interval_map[period]
-    prices = closes_df[product['标的']].values
-    changes = prices[intervals[1]] / prices[intervals[0]]
-    bools = changes < low_bound if product['方向'] == '看涨' else changes > low_bound
-    n_lower = np.sum(changes < low_bound if product['方向'] == '看涨' else changes > low_bound)
-    nnull = closes_df[product['标的']].isnull().sum()
-    return n_lower / (len(changes) - closes_df[product['标的']].isnull().sum())
-
-
-def backtest(closes_df, product):
-    period = np.floor(product['期限(月)'])
-    intervals = interval_map[period]
-    prices0 = closes_df[product['标的']].values[intervals[0]]
-    prices1 = closes_df[product['标的']].values[intervals[1]]
-    changes = prices1 / prices0
-    struct, low_bound, direction = product['结构'], product['下端收益触达线'], product['方向']
-
-    if struct == '两区间' or struct == '三区间' or struct == '价差' or struct == '触碰式':
-        return bt_plain(closes_df, product)
-    elif struct == '鲨鱼鳍':
-        if direction == '两边':
+    def bt_shark_fin(self, product):
+        if product['方向'] == '两边':
             return None
         else:
-            n_lower = np.sum(changes < low_bound if direction == '看涨' else changes > low_bound)
-        return n_lower / len(changes)
-    elif struct == '区间累计':
-        lows = [float(x.replace('%', 'e-2')) for x in str(low_bound).split('；')]
-        n_lower = np.sum((changes < lows[0]) | (changes > lows[1]))
-        return n_lower / len(changes)
-    elif struct == '自动敲出':
-        return auto_call(closes_df, product, direction == '看涨')
-    return None
+            low_bound = product['下端收益触达线']
+            intervals = self.interval_map[np.floor(product['期限(月)'])]
+            prices = self.closes_df[product['标的']].values
+            ratios = prices[intervals[1]] / prices[intervals[0]]
+            n_lower = np.sum(ratios < low_bound if product['方向'] == '看涨' else ratios > low_bound)
+            return n_lower / len(ratios)
 
+    def bt_range_accrual(self, product):
+        intervals = self.interval_map[np.floor(product['期限(月)'])]
+        prices = self.closes_df[product['标的']].values
+        ratios = prices[intervals[1]] / prices[intervals[0]]
 
-def cal_lower_p(product, closes_df):
-    if product['结构'] not in structs or product['标的'] not in underlyings or product['期限(月)'] not in interval_map:
-        return None
-    else:
-        return backtest(closes_df, product)
+        price_range = [float(x.replace('%', 'e-2')) for x in str(product['下端收益触达线']).split('；')]
+        n_lower = np.sum((ratios < price_range[0]) | (ratios > price_range[1]))
+        return n_lower / len(ratios)
+
+    def backtest(self, product):
+        struct = product['结构']
+        return self.bt_map[struct](product) if struct in self.bt_map else None
+
+    def cal_lower_p(self, product):
+        if product['结构'] not in self.structs or product['标的'] not in self.underlyings or product[
+            '期限(月)'] not in self.interval_map:
+            return None
+        else:
+            return self.backtest(product)
+
+    def run(self):
+        return self.products.apply(self.cal_lower_p, axis=1)
 
 
 if __name__ == '__main__':
     fp = 'D:\\OneDrive\\Intern\\CIB\\Work\\同业结构\\0224\\结构性产品同业发行结构汇总20230222.xlsx'
-    products = pd.read_excel(fp, sheet_name='历史发行结构汇总', usecols='H:M', skiprows=2)
-    closes_df = pd.read_excel(fp, sheet_name='历史走势')
-
-    products['期限(月)'] = np.floor(products['期限(月)'])
-    underlyings = set(closes_df.columns)
-    gen_intervals(closes_df)
-
-    results = products.apply(cal_lower_p, closes_df=closes_df, axis=1)
+    tester = LowerBoundTester(fp)
+    results = tester.run()
     print(results)
 
     # xw.Book(fp).sheets['历史发行结构汇总']['O4'].options(index=False, header=False).value = results
