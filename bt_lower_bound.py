f922f8b4f62c4804a39d34a8a0b82a11c6c68505
Ranger Shaw <rangershaw96@outlook.com>
2023-02-27 15:53:07 +0800

    get_intervals

--- bt_lower_bound.py
+++ bt_lower_bound.py
@@ -7,31 +7,31 @@ class LowerBoundTester:
 
     def __init__(self, fp):
         self.bt_map = {
+            '价差': self.bt_plain,
             '两区间': self.bt_plain,
             '三区间': self.bt_plain,
-            '价差': self.bt_plain,
-            '触碰式': self.bt_touch,
             '鲨鱼鳍': self.bt_shark_fin,
+            '触碰式': self.bt_touch,
             '区间累计': self.bt_range_accrual,
             '自动敲出': self.bt_auto_call,
         }
         self.products = pd.read_excel(fp, sheet_name='历史发行结构汇总', usecols='H:M', skiprows=2)
         self.closes_df = pd.read_excel(fp, sheet_name='历史走势')
-        self.products['期限(月)'] = np.floor(self.products['期限(月)'])
-        self.interval_map = self.gen_intervals()
+        self.interval_map = self.build_intervals()
+        self.period_months = np.array(list(self.interval_map.keys()))
 
-    def gen_intervals(self):
+    def build_intervals(self):
         interval_map = {}
-        period_months = [0, 1, 2, 3, 6, 9, 12, 36]  # 0 is 14d
-        back_years = [0.5, 1, 2, 3, 5, 9, 10, 13]
+        period_months = [0.25, 0.5, 1, 2, 3, 6, 9, 12, 36]
+        back_years = [0.25, 0.5, 1, 2, 3, 5, 9, 10, 13]
         date_sr = self.closes_df['日期']
         today = date_sr.iloc[-1]
         print(f'today: {today}')
 
         for i, month in enumerate(period_months):
-            period = pd.DateOffset(months=month) if i != 0 else pd.DateOffset(days=14)
-
-            fst_sdate = today - (pd.DateOffset(years=back_years[i]) if i != 0 else pd.DateOffset(months=6))
+            period = pd.DateOffset(months=month) if month >= 1 else pd.DateOffset(days=round(28 * month))
+            year = back_years[i]
+            fst_sdate = today - (pd.DateOffset(years=year) if year >= 1 else pd.DateOffset(months=round(year * 12)))
             lst_sdate = today - period
             i_fst_sdate = (date_sr >= fst_sdate).argmax()
             i_lst_sdate = (date_sr <= lst_sdate).argmin()
@@ -43,35 +43,59 @@ class LowerBoundTester:
             bools = date_sr.values >= edates
             intervals[1] = np.argmax(bools, axis=1)
             print(len(intervals[0]))
-            interval_map[period_months[i]] = intervals
+            interval_map[month] = intervals
 
         return interval_map
 
+    def get_intervals(self, period, prices: np.ndarray):
+        if period not in self.period_months:
+            idx = (np.abs(self.period_months - period)).argmin()
+            period = self.period_months[idx]  # nearest period
+        intervals = self.interval_map[period]
+        n_nan = np.isnan(prices[intervals[0]]).sum()
+        return intervals[:, n_nan:]
+
     def get_ratios(self, product: pd.Series):
-        intervals = self.interval_map[np.floor(product['期限(月)'])]
         prices = self.closes_df[product['标的']].values
-        prices0 = prices[intervals[0]]
-        ratios = prices[intervals[1]] / prices0
-        return ratios, prices0
+        intervals = self.get_intervals(product['期限(月)'], prices)
+        ratios = prices[intervals[1]] / prices[intervals[0]]
+        return ratios
+
+    def bt_plain(self, product):
+        ratios = self.get_ratios(product)
+        low_bound = product['下端收益触达线']
+        n_lower = np.sum(ratios < low_bound if product['方向'] == '看涨' else ratios > low_bound)
+        return n_lower / (len(ratios))
+
+    def bt_shark_fin(self, product):
+        if product['方向'] == '两边':
+            return None
+        ratios = self.get_ratios(product)
+        low_bound = product['下端收益触达线']
+        n_lower = np.sum(ratios < low_bound if product['方向'] == '看涨' else ratios > low_bound)
+        return n_lower / len(ratios)
+
+    def bt_range_accrual(self, product):
+        ratios = self.get_ratios(product)
+        price_range = [float(x.replace('%', 'e-2')) for x in str(product['下端收益触达线']).split('；')]
+        n_lower = np.sum((ratios < price_range[0]) | (ratios > price_range[1]))
+        return n_lower / len(ratios)
 
     def bt_auto_call(self, product):
-        period = np.floor(product['期限(月)'])
-        intervals = self.interval_map[period]
         prices = self.closes_df[product['标的']].values
+        intervals = self.get_intervals(product['期限(月)'], prices)
 
         i_odates = np.linspace(intervals[0], intervals[1], 13, axis=1).round().astype(int)
         paths = prices[i_odates[:, 1:]]
-        kout_prices = (prices[i_odates[:, 0]] * product['下端收益触达线']).reshape(paths.shape[0], 1)
-
+        kout_prices = prices[i_odates[:, 0], None] * product['下端收益触达线']
         nkout_bools = paths < kout_prices if product['方向'] == '看涨' else paths > kout_prices
         n_nkout = np.all(nkout_bools, axis=1).sum()
         return n_nkout / len(intervals[0])
 
     def bt_touch(self, product):
-        period = np.floor(product['期限(月)'])
-        is_call = product['方向'] == '看涨' or product['方向'] == '触入看涨'
-        intervals = self.interval_map[period]
         prices = self.closes_df[product['标的']].values
+        intervals = self.get_intervals(product['期限(月)'], prices)
+        is_call = product['方向'] == '看涨' or product['方向'] == '触入看涨'
         low_prices = product['下端收益触达线'] * prices[intervals[0]]
 
         n_low = 0
@@ -81,32 +105,10 @@ class LowerBoundTester:
                 n_low += 1
         return n_low / len(intervals[0])
 
-    def bt_plain(self, product):
-        ratios, prices0 = self.get_ratios(product)
-        low_bound = product['下端收益触达线']
-        n_lower = np.sum(ratios < low_bound if product['方向'] == '看涨' else ratios > low_bound)
-        return n_lower / (len(ratios) - np.isnan(prices0).sum())  # 民生全球
-
-    def bt_shark_fin(self, product):
-        if product['方向'] == '两边':
-            return None
-        ratios, _ = self.get_ratios(product)
-        low_bound = product['下端收益触达线']
-        n_lower = np.sum(ratios < low_bound if product['方向'] == '看涨' else ratios > low_bound)
-        return n_lower / len(ratios)
-
-    def bt_range_accrual(self, product):
-        ratios, _ = self.get_ratios(product)
-        price_range = [float(x.replace('%', 'e-2')) for x in str(product['下端收益触达线']).split('；')]
-        n_lower = np.sum((ratios < price_range[0]) | (ratios > price_range[1]))
-        return n_lower / len(ratios)
-
     def backtest(self, product):
-        if not (product['结构'] in self.bt_map and product['标的'] in self.closes_df.columns
-                and product['期限(月)'] in self.interval_map):
+        if product['结构'] not in self.bt_map or product['标的'] not in self.closes_df.columns:
             return None
-        else:
-            return self.bt_map[product['结构']](product)
+        return self.bt_map[product['结构']](product)
 
     def run(self):
         return self.products.apply(self.backtest, axis=1)
